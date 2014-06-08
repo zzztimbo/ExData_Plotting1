6ba08531d421adfa9f4bc55605eef97e4f240b1b
Tim Chan <tchan@edmunds.com>
2014-06-08 13:05:26 -0700

    finished histogram

---
+++ plot4.R
@@ -0,0 +1,28 @@
+# obtain data
+fileNameCleanDates <- "household_power_consumption.csv"
+if (!file.exists(fileNameCleanDates)) {
+   fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+   fileNameZipped <- "household_power_consumption.zip" 
+   fileNameUnzipped <- "household_power_consumption.txt"
+   
+   if (!file.exists(fileNameZipped)) { 
+      print("downloading dataset...")
+      download.file(fileUrl, destfile = fileNameZipped, method = "curl") 
+   }
+   
+   if (!file.exists(fileNameUnzipped)) { 
+      print("unzipping dataset...")
+      unzip(fileNameZipped)
+   }
+   
+   # filter on date range
+   household.power.consumption <- read.csv(fileNameUnzipped, sep=";", header=TRUE)
+   household.power.consumption$Date <- as.Date(as.character(household.power.consumption$Date), "%d/%m/%Y")
+   household.power.consumption <- subset(household.power.consumption, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+   
+   write.csv(household.power.consumption, file=fileNameCleanDates)
+} else {
+   household.power.consumption <- read.csv(fileNameCleanDates, header=TRUE)   
+}
+
+
